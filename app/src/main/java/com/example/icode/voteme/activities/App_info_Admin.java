e9e955fb756fa9bc6f794e2dac392b7b06401535
ALI-SAMPSON <alisampson2@gmail.com>
2018-04-02 13:21:40 -0700

    Updated

--- app/src/main/java/com/example/icode/voteme/activities/App_info_Admin.java
+++ app/src/main/java/com/example/icode/voteme/activities/App_info_Admin.java
@@ -1,8 +1,10 @@
 package com.example.icode.voteme.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.icode.voteme.R;
 
@@ -15,5 +17,26 @@ public class App_info_Admin extends AppCompatActivity {
         Toolbar toolbar =(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("ABOUT US");
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_left_black);
+
+        //Enables the back button on the toolbar
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //Home Button
+                Intent homeIntent = new Intent(this, AdminPanel.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
